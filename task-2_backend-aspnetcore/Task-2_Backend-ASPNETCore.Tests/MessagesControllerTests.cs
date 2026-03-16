7c54218707c8421cd10f56f5d75fcf716b0e8fba
Erand Braho <erandbraho@yahoo.com>
2026-03-16 14:50:29 +0100

    Add tests for AdministrativeChangeMessage endpoints

--- task-2_backend-aspnetcore/Task-2_Backend-ASPNETCore.Tests/MessagesControllerTests.cs
+++ task-2_backend-aspnetcore/Task-2_Backend-ASPNETCore.Tests/MessagesControllerTests.cs
@@ -11,6 +11,8 @@ public class MessagesControllerTests(CustomWebApplicationFactory factory) : ICla
 {
     private readonly HttpClient _client = factory.CreateClient();
 
+    #region GetMessages
+
     [Fact]
     public async Task GetMessages_ReturnsEmptyList_WhenNoMessagesExist()
     {
@@ -25,6 +27,7 @@ public class MessagesControllerTests(CustomWebApplicationFactory factory) : ICla
         var result = await response.Content.ReadFromJsonAsync<MessageCollectionDto>();
         result.Should().NotBeNull();
         result.QuestionMessages.Should().BeEmpty();
+        result.AdministrativeChangeMessages.Should().BeEmpty();
     }
 
     [Fact]
@@ -40,6 +43,10 @@ public class MessagesControllerTests(CustomWebApplicationFactory factory) : ICla
             "/messages/question",
             new QuestionMessageCreateDto { Question = "Test Question 2" }
         );
+        await _client.PostAsJsonAsync(
+            "/messages/administrative-change",
+            ValidAdministrativeChangeMessageCreateDto()
+        );
 
         // Act
         var response = await _client.GetAsync("/messages");
@@ -51,6 +58,8 @@ public class MessagesControllerTests(CustomWebApplicationFactory factory) : ICla
         result.QuestionMessages.Should().HaveCount(2);
         result.QuestionMessages.Select(m => m.Question).Should().Contain("Test Question 1");
         result.QuestionMessages.Select(m => m.Question).Should().Contain("Test Question 2");
+        result.AdministrativeChangeMessages.Should().HaveCount(1);
+        result.AdministrativeChangeMessages.First().LastName.Should().Be("Last");
     }
 
     [Fact]
@@ -66,6 +75,10 @@ public class MessagesControllerTests(CustomWebApplicationFactory factory) : ICla
         response.Content.Headers.ContentType.Should().NotBeNull();
         response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
     }
+
+    #endregion
+
+    #region PostQuestionMessage
 
     [Fact]
     public async Task PostQuestionMessage_ReturnsCreatedMessage_WithValidData()
@@ -137,11 +150,303 @@ public class MessagesControllerTests(CustomWebApplicationFactory factory) : ICla
         response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
     }
 
+    #endregion
+
+    #region PostAdministrativeChangeMessage
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsCreatedMessage_WithValidData()
+    {
+        // Arrange
+        await CleanDatabase();
+
+        // Act
+        var response = await _client.PostAsJsonAsync(
+            "/messages/administrative-change",
+            ValidAdministrativeChangeMessageCreateDto()
+        );
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var result = await response.Content.ReadFromJsonAsync<AdministrativeChangeMessageDto>();
+        result.Should().NotBeNull();
+        result.Id.Should().BeGreaterThan(0);
+        result.FirstName.Should().Be("First");
+        result.LastName.Should().Be("Last");
+        result.PostalAddress.Street.Should().Be("Haspelstrasse");
+        result.PostalAddress.HouseNumber.Should().Be(5);
+        result.PostalAddress.ZipCode.Should().Be("8041");
+        result.PostalAddress.City.Should().Be("Zürich");
+        result.PostalAddress.Canton.Should().Be("Zürich");
+        result.FreeText.Should().Be("Address change requested.");
+    }
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsBadRequest_WithMissingFirstName()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.FirstName = "";
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostQuestionMessage_ReturnsBadRequest_WithTooLongFirstName()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.FirstName = new string('a', 101);
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsBadRequest_WithMissingLastName()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.LastName = "";
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostQuestionMessage_ReturnsBadRequest_WithTooLongLastName()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.LastName = new string('a', 101);
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsBadRequest_WithMissingStreet()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.PostalAddress.Street = "";
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostQuestionMessage_ReturnsBadRequest_WithTooLongStreet()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.PostalAddress.Street = new string('a', 101);
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsBadRequest_WithZeroHouseNumber()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.PostalAddress.HouseNumber = 0;
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsBadRequest_WithNegativeHouseNumber()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.PostalAddress.HouseNumber = -1;
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsBadRequest_WithInvalidZipCode()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.PostalAddress.ZipCode = "abc";
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsBadRequest_WithMissingCity()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.PostalAddress.City = "";
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostQuestionMessage_ReturnsBadRequest_WithTooLongCity()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.PostalAddress.City = new string('a', 101);
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsBadRequest_WithMissingCanton()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.PostalAddress.Canton = "";
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostQuestionMessage_ReturnsBadRequest_WithTooLongCanton()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.PostalAddress.Canton = new string('a', 101);
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsBadRequest_WithMissingFreeText()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.FreeText = "";
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsBadRequest_WithTooLongFreeText()
+    {
+        // Arrange
+        await CleanDatabase();
+        var dto = ValidAdministrativeChangeMessageCreateDto();
+        dto.FreeText = new string('a', 1025);
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/messages/administrative-change", dto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task PostAdministrativeChangeMessage_ReturnsJsonContentType()
+    {
+        // Arrange
+        await CleanDatabase();
+
+        // Act
+        var response = await _client.PostAsJsonAsync(
+            "/messages/administrative-change",
+            ValidAdministrativeChangeMessageCreateDto()
+        );
+
+        // Assert
+        response.Content.Headers.ContentType.Should().NotBeNull();
+        response.Content.Headers.ContentType!.MediaType.Should().Be("application/json");
+    }
+
+    #endregion
+
+    private static AdministrativeChangeMessageCreateDto ValidAdministrativeChangeMessageCreateDto() =>
+    new()
+    {
+        FirstName = "First",
+        LastName = "Last",
+        PostalAddress = new PostalAddress
+        {
+            Street = "Haspelstrasse",
+            HouseNumber = 5,
+            ZipCode = "8041",
+            City = "Zürich",
+            Canton = "Zürich"
+        },
+        FreeText = "Address change requested."
+    };
+
     private async Task CleanDatabase()
     {
         using var scope = factory.Services.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<MessagesContext>();
         context.QuestionMessages.RemoveRange(context.QuestionMessages);
+        context.AdministrativeChangeMessages.RemoveRange(context.AdministrativeChangeMessages);
         await context.SaveChangesAsync();
     }
 }
